c755819ff3220e310d9a2afdf968df67e2a45760
Keisuke Taniguchi <rememberthatk@icloud.com>
2020-12-04 09:43:41 +0900

    update

--- config.fish
+++ config.fish
@@ -60,3 +60,27 @@ status --is-interactive; and source (rbenv init -|psub)
 set -g theme_powerline_fonts no
 set -g theme_nerd_fonts yes
 set -g theme_display_nvm yes
+
+function bind_bang
+    switch (commandline -t)[-1]
+        case "!"
+            commandline -t $history[1]; commandline -f repaint
+        case "*"
+            commandline -i !
+    end
+end
+
+function bind_dollar
+    switch (commandline -t)[-1]
+        case "!"
+            commandline -t ""
+            commandline -f history-token-search-backward
+        case "*"
+            commandline -i '$'
+    end
+end
+
+function fish_user_key_bindings
+    bind ! bind_bang
+    bind '$' bind_dollar
+end
